d70f665e52ee87f29cb48ab55eac2a260dad7612
aldanamichelino <aldana.michelino@gmail.com>
2019-08-07 06:14:34 -0300

    rutas

---
+++ routes/web.php
@@ -0,0 +1,118 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/index', function () {
+    return view('index');
+});
+
+Route::get('/preguntasFrecuentes', function () {
+    return view('preguntasFrecuentes');
+});
+
+Route::get('/registro', function () {
+    return view('registro');
+});
+
+Route::post('/registro', function () {
+    return view('registro');
+});
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::post('/login', function () {
+    return view('login');
+});
+
+Route::get('/perfil{id}', function ($id) {
+    $vac = compact("id");
+    return view('perfil', $vac);
+});
+
+//no estoy segura de si perfil solo sería por post o no
+
+Route::post('/perfil{id}', function ($id) {
+    $vac = compact("id");
+    return view('perfil', $vac);
+});
+
+Route::get('/juego', function () {
+    return view('juego');
+});
+
+Route::get('/administrador', function () {
+    return view('administrador');
+});
+
+Route::get('/acceso', function () {
+    return view('acceso');
+});
+
+Route::get('/listadoSeriesAdmin', function () {
+    return view('listadoSeriesAdmin');
+});
+
+Route::get('/agregarSerie', function () {
+    return view('agregarSerie');
+});
+
+Route::post('/agregarSerie', function () {
+    return view('agregarSerie');
+});
+
+Route::get('/agregarPreguntas', function () {
+    return view('agregarPreguntas');
+});
+
+Route::post('/agregarPreguntas', function () {
+    return view('agregarPreguntas');
+});
+
+Route::get('/eliminarSeriesAdmin/{id}', function ($id) {
+    $vac = compact("id");
+    return view('eliminarSeriesAdmin', $vac);
+});
+
+Route::post('/eliminarSeriesAdmin{id}', function ($id) {
+    $vac = compact("id");
+    return view('eliminarSeriesAdmin', $vac);
+});
+
+Route::get('/administradorpreguntasfrecuentes', function () {
+    return view('administradorpreguntasfrecuentes');
+});
+
+Route::get('/editarPreguntaFrecuente{id}', function ($id) {
+    $vac = compact("id");
+    return view('editarPreguntaFrecuente'. $vac);
+});
+
+Route::post('/editarPreguntaFrecuente{id}', function ($id) {
+    $vac = compact("id");
+    return view('editarPreguntaFrecuente'. $vac);
+});
+
+Route::get('/eliminarPreguntaFrecuente{id}', function ($id) {
+    $vac = compact("id");
+    return view('eliminarPreguntaFrecuente', $vac);
+});
+
+Route::post('/eliminarPreguntaFrecuente{id}', function () {
+  $vac = compact("id");
+    return view('eliminarPreguntaFrecuente', $vac);
+});
