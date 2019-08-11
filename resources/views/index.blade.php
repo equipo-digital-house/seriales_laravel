afc039e48662645c5928fc92bc7e781e90a1ccc1
Aldana Michelino <aldana.michelino@gmail.com>
2019-08-11 10:36:16 -0300

    carousel index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,4 +1,4 @@
-@include('layouts/master')
+@extends('layouts/master')
 
 @section('content')
 
@@ -6,33 +6,44 @@
 <html lang="es" dir="ltr">
 
 <body>
-  <div id="carouselSeries" class="carousel slide" data-ride="carousel">
-    <div class="carousel-inner">
-      <div class="carousel-item active" data-interval="5000">
-        <img src="/imgsitio/gameOfThrones.png" class="d-block w-100" alt="GOT">
+  <main class="inicio">
+    <div id="carouselSeries" class="carousel slide" data-ride="carousel">
+      <div class="carousel-inner">
+        <div class="carousel-item active" data-interval="5000">
+          <img src="/imgsitio/explicacion1.png" class="d-block w-100" alt="Seriales">
+        </div>
+        <div class="carousel-item" data-interval="4000">
+          <img src="/imgsitio/gameOfThrones.png" class="d-block w-100" alt="GOT">
+        </div>
+        <div class="carousel-item" data-interval="4000">
+          <img src="/imgsitio/orangeIsTheNewBlack.png" class="d-block w-100" alt="OITNB">
+        </div>
+        <div class="carousel-item" data-interval="4000">
+          <img src="/imgsitio/seinfeld.png" class="d-block w-100" alt="Seinfeld">
+        </div>
+        <div class="carousel-item" data-interval="4000">
+          <img src="/imgsitio/theWalkingDead.png" class="d-block w-100" alt="TWD">
+        </div>
+        <div class="carousel-item" data-interval="4000">
+          <img src="/imgsitio/friends.png" class="d-block w-100" alt="Friends">
+        </div>
+        <div class="carousel-item" data-interval="4000">
+          <img src="/imgsitio/vikings.png" class="d-block w-100" alt="Wikings">
+        </div>
+        <div class="carousel-item" data-interval="4000">
+          <img src="/imgsitio/theBigBangTheory.png" class="d-block w-100" alt="TBBT">
+        </div>
       </div>
-      <div class="carousel-item" data-interval="5000">
-        <img src="/imgsitio/orangeIsTheNewBlack.png" class="d-block w-100" alt="OITNB">
-      </div>
-      <div class="carousel-item" data-interval="5000">
-        <img src="/imgsitio/Seinfeld.png" class="d-block w-100" alt="Seinfeld">
-      </div>
-      <div class="carousel-item" data-interval="5000">
-        <img src="/imgsitio/theWalkingDead.png" class="d-block w-100" alt="TWD">
-      </div>
-      <div class="carousel-item" data-interval="5000">
-        <img src="/imgsitio/Vikings.png" class="d-block w-100" alt="Wikings">
-      </div>
-    </div>
-    <a class="carousel-control-prev" href="#carouselExampleInterval" role="button" data-slide="prev">
-      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-      <span class="sr-only">Previous</span>
-    </a>
-    <a class="carousel-control-next" href="#carouselExampleInterval" role="button" data-slide="next">
-      <span class="carousel-control-next-icon" aria-hidden="true"></span>
-      <span class="sr-only">Next</span>
-    </a>
+      <a class="carousel-control-prev" href="#carouselSeries" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="carousel-control-next" href="#carouselSeries" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
   </div>
+</main>
 
 </body>
 
