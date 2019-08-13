e79c97590541f159f11a3094102cab8eab555cfe
cpinot <cynthia.priscila.pino@gmail.com>
2019-08-13 11:37:19 -0300

    Administrador de series, cambio en los datos imagen de json

--- resources/views/series/listadoSeries.blade.php
+++ resources/views/series/listadoSeries.blade.php
@@ -1,13 +1,6 @@
-<!DOCTYPE html>
-<html lang="es" dir="ltr">
-  <head>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="/css/master.css">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
-    <meta charset="utf-8">
-    <title></title>
-  </head>
-<body>
+@extends('layouts.admin')
+@section('content')
+
     <div class="container">
           <div class="alert alert-dark" role="alert">
           <h2 class="display-6 text-center">Listado de Series - Preguntas y Respuestas</h2>
@@ -34,7 +27,7 @@
                   <th scope="row">{{$cont++}}</th>
 
                    @if($serie->image!=NULL)
-                     <td><img src="{{$serie->image}}" alt="" class="img-fluid rounded" width="100px"></td>
+                     <td><img src="/storage/img/series/{{$serie->image}}" alt="" class="img-fluid rounded" width="100px"></td>
                    @else
                    <td>Sin Imagen</td>
                    @endif
@@ -61,18 +54,13 @@
 
   <ul class="nav justify-content-end">
   <li class="nav-item">
-    <a class="nav-link" href="administrador.php"><i class="fas fa-arrow-circle-left" alt="Retornar"></i>Retornar</a>
+    <a class="nav-link" href="/administrador"><i class="fas fa-arrow-circle-left" alt="Retornar"></i>Retornar</a>
   </li>
 
   <li class="nav-item">
     <a class="nav-link" href="/nuevaSerie"><i class="fas fa-plus-circle"></i>Nueva Serie</a>
   </li>
 
-</ul>
+   </ul>
 
-  </body>
-  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-
-</html>
+@endsection
