017bcf9350664875e89dbc0fca565ea60ad12fc2
Aldana Michelino <aldana.michelino@gmail.com>
2019-08-13 23:33:08 -0300

    crud faq funcional

--- resources/views/preguntasfrecuentes/administradorpreguntasfrecuentes.blade.php
+++ resources/views/preguntasfrecuentes/administradorpreguntasfrecuentes.blade.php
@@ -54,7 +54,7 @@
                           <a href="/editarpreguntafrecuente/update/{{$pregunta->id}}"><i class="fas fa-edit"></i></a>
                       </td>
                       <td>
-                          <a href="/eliminarpreguntafrecuente/{{$pregunta->id}}"><i class="fas fa-trash"></i></a>
+                          <a href="/borrarFaq/{{$pregunta->id}}"><i class="fas fa-trash"></i></a>
                       </td>
                   </tr>
                   @endforeach
